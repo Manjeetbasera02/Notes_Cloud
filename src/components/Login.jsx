981aa1df151c23fc705f8d0b2980505f3d31e8f6
Manjeet <manjeetman38@gmail.com>
2024-09-27 16:38:41 +0530

    login and signup implementation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+    const [data, setdata] = useState({
+        email: "",
+        password: ""
+    })
+
+    const handlechange = (e) => {
+        const {name, value} = e.target
+
+        setdata((prevdata) => {
+            return {...prevdata, [name]: value}
+        })
+    }
+
+    const handlesubmit = (e) => {
+        e.preventDefault()
+        console.log(data)
+    }
+
   return (
-    <div>Login</div>
+    <>
+    <form onSubmit={handlesubmit}>
+        <div>
+            <label htmlFor="email">Email</label>
+            <input type="text" id='email' name='email' value={data.email} onChange={handlechange} />
+        </div>
+
+        <div>
+            <label htmlFor="">Password</label>
+            <input type="password" name='password' value={data.password} onChange={handlechange} />
+        </div>
+
+        <div>
+            <button type='submit'>Login</button>
+        </div>
+    </form>
+    </>
   )
 }
 
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,8 +1,52 @@
+import { setDriver } from 'mongoose'
 import React from 'react'
+import { useState } from 'react'
 
 function Signup() {
+  const [data, setdata] = useState({
+    name: "",
+    email: "",
+    password: ""
+  })
+
+  const handlechange = (e) => {
+    const {name, value} = e.target
+    setdata((prevdata) => {
+      // use dynamic key [name]
+      return {...prevdata, [name]: value}
+    })
+  }
+
+  const handlesubmit = (e) => {
+    console.log('form submitted', data)
+    e.preventDefault()
+  }
+  
   return (
-    <div>Signup</div>
+    <>
+
+    <form onSubmit={handlesubmit}>
+      <div>
+        <label htmlFor="username">Name</label>
+        <input type="text" id='username' name='name' value={data.name} onChange={handlechange} />
+      </div>
+
+      <div>
+        <label htmlFor="email">Email</label>
+        <input type="text" id='email' name='email' value={data.email} onChange={handlechange}/>
+      </div>
+
+      <div>
+        <label htmlFor="password">Password</label>
+        <input type="password" id='password' name='password' value={data.password} onChange={handlechange}/>
+      </div>
+
+      <div>
+        <button type='submit' >Signup</button>
+      </div>
+    </form>
+
+    </>
   )
 }
 
